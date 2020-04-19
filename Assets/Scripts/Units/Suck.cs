f3df9d0a2347ca00f384507a4088106c5a8d1afb
Rexagon <reide740@gmail.com>
2020-04-19 23:16:55 +0300

    Add walk animation

---
+++ Assets/Scripts/Units/Suck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suck : MonoBehaviour
+{
+    public WaterArea waterArea;
+    public float cooldown = 10;
+
+    public float _currentCooldown = 0;
+
+    void FixedUpdate()
+    {
+        _currentCooldown -= Time.fixedDeltaTime;
+
+        if (_currentCooldown <= 0)
+        {
+            _currentCooldown = cooldown;
+            waterArea.Evaporate(transform.position);
+        }
+    }
+}
