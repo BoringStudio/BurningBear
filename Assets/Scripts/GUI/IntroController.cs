c7d284eb96a581024cb37aea3dc296b3d09aaf98
Rexagon <reide740@gmail.com>
2020-04-21 00:29:58 +0300

    Add intro and main menu

---
+++ Assets/Scripts/GUI/IntroController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.GUI
+{
+    class IntroController : MonoBehaviour
+    {
+        public string nextSceneName;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                LoadMain();
+            }
+        }
+
+        public void LoadMain()
+        {
+            SceneManager.LoadScene(nextSceneName, LoadSceneMode.Single);
+        }
+    }
+}
---
+++ Assets/Scripts/GUI/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Menu
+{
+    class MenuController : MonoBehaviour
+    {
+        public string nextSceneName;
+
+        public void LoadIntro()
+        {
+            SceneManager.LoadScene(nextSceneName, LoadSceneMode.Additive);
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
