cd3502f769ab649a289c286470a48e9a8f9b2f3c
LazyMechanic <AsharnRus@gmail.com>
2020-04-21 03:22:58 +0300

    Add GameOver

---
+++ Assets/Scripts/Utils/GameOverController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour
+{
+    public float maxGameTime = 600.0f;
+    private float _curTimer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _curTimer += Time.deltaTime;
+        if (_curTimer >= maxGameTime)
+        {
+            DoGameOver();
+        }
+    }
+
+    public void DoGameOver()
+    {
+        SceneManager.LoadScene("GameOverScene", LoadSceneMode.Additive);
+    }
+}
